d434406ec98924b699f042216e098733a5295434
Milko Škofič <milko@skofic.io>
2024-10-22 20:16:45 +0200

    Implemented database credentials.
    Credentials are now in a separate globals file, excluded by gitignore.

---
+++ globals.localhost.js
@@ -0,0 +1,511 @@
+'use strict'
+
+/**
+ * globals.example.js
+ *
+ * This file contains the global variables used in the project.
+ * This is an example file, so the actual references are just
+ * for example purposes. Copy this file and name the copy, for
+ * instance, globals.localhost.js for accessing the localhost
+ * database.
+ */
+
+///
+// Includes.
+///
+const { credentials } = require("./credentials.localhost.js")
+
+
+///
+// Globals.
+///
+module.exports = Object.freeze({
+	// Database connection.
+	db: credentials,
+	
+	// Collection names.
+	collections: {
+		chelsa: 'Chelsa',
+		genus: 'EU-Forest_Genus',
+		species: 'EU-Forest_Species',
+		location: 'EU-Forest_Occurrences',
+		work: 'EU-Forest_Work',
+		work_eufgis: 'EUFGIS_Work',
+		final: 'EU-Forest_Chelsa',
+		final_enfgis: 'EUFGIS_Chelsa',
+		stats: 'Stats'
+	},
+	
+	// Collection indexes.
+	indexes: {
+		chelsa: [
+		],
+		genus: [
+			{
+				"name": "idx_genera",
+				"type": "persistent",
+				"fields": ["properties.genus[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_geometry",
+				"type": "geo",
+				"fields": ["geometry"],
+				"geoJson": true,
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			}
+		],
+		species: [
+			{
+				"name": "idx_geometry",
+				"type": "geo",
+				"fields": ["geometry"],
+				"geoJson": true,
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_species",
+				"type": "persistent",
+				"fields": ["properties.species"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			}
+		],
+		location: [
+			{
+				"name": "idx_geometry",
+				"type": "geo",
+				"fields": ["geometry"],
+				"geoJson": true,
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_species",
+				"type": "persistent",
+				"fields": ["properties.species_list[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			}
+		],
+		work: [
+			{
+				"name": "idx_geometry",
+				"type": "geo",
+				"fields": ["geometry"],
+				"geoJson": true,
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_geometry_hash",
+				"type": "persistent",
+				"fields": ["geometry_hash"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_species",
+				"type": "persistent",
+				"fields": ["properties.species_list[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			}
+		],
+		work_eufgis: [
+			{
+				"name": "idx_geometry_hash",
+				"type": "persistent",
+				"fields": ["geometry_hash"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_gcu_id_number",
+				"type": "persistent",
+				"fields": "gcu_id_number",
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_gcu_id_unit-id",
+				"type": "persistent",
+				"fields": "gcu_id_unit-id",
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			}
+		],
+		final: [
+			{
+				"name": "idx_species",
+				"type": "persistent",
+				"fields": ["properties.species_list[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_pr",
+				"type": "persistent",
+				"fields": ["properties.env_climate_pr"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_tas",
+				"type": "persistent",
+				"fields": ["properties.env_climate_tas"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio01",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio01"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio04",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio04"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio05",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio05"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio06",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio06"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio12",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio12"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio15",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio15"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_vpd_mean",
+				"type": "persistent",
+				"fields": ["properties.env_climate_vpd_mean"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			}
+		],
+		final_eufgis: [
+			{
+				"name": "idx_gcu_id_number",
+				"type": "persistent",
+				"fields": ["properties.gcu_id_number_list[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_gcu_id_unit-id",
+				"type": "persistent",
+				"fields": ["properties.`gcu_id_unit-id_list`[*]"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": false,
+				"unique": false
+			},
+			{
+				"name": "idx_pr",
+				"type": "persistent",
+				"fields": ["properties.env_climate_pr"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_tas",
+				"type": "persistent",
+				"fields": ["properties.env_climate_tas"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio01",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio01"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio04",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio04"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio05",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio05"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio06",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio06"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio12",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio12"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_bio15",
+				"type": "persistent",
+				"fields": ["properties.env_climate_bio15"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			},
+			{
+				"name": "idx_vpd_mean",
+				"type": "persistent",
+				"fields": ["properties.env_climate_vpd_mean"],
+				"estimates": true,
+				"cacheEnabled": false,
+				"deduplicate": false,
+				"sparse": true,
+				"unique": false
+			}
+		],
+		stats: [
+		]
+	},
+	pairs: {
+		tas_pr: {
+			key: "tas_pr",
+			X: "env_climate_tas",
+			Y: "env_climate_pr",
+			collections: [
+				{
+					name: "tas_pr_chelsa_full",
+					species: false,
+					units: false
+				},
+				{
+					name: "tas_pr_eu_full",
+					species: true,
+					units: false
+				},
+				{
+					name: "tas_pr_eufgis_full",
+					species: false,
+					units: true
+				},
+				{
+					name: "tas_pr_chelsa_round",
+					species: false,
+					units: false
+				},
+				{
+					name: "tas_pr_eu_round",
+					species: true,
+					units: false
+				},
+				{
+					name: "tas_pr_eufgis_round",
+					species: false,
+					units: true
+				},
+			]
+		},
+		bio01_bio12: {
+			key: "bio01_bio12",
+			X: "env_climate_bio01",
+			Y: "env_climate_bio12",
+			collections: [
+				{
+					name: "bio01_bio12_chelsa_full",
+					species: false,
+					units: false
+				},
+				{
+					name: "bio01_bio12_eu_full",
+					species: true,
+					units: false
+				},
+				{
+					name: "bio01_bio12_eufgis_full",
+					species: false,
+					units: true
+				},
+				{
+					name: "bio01_bio12_chelsa_round",
+					species: false,
+					units: false
+				},
+				{
+					name: "bio01_bio12_eu_round",
+					species: true,
+					units: false
+				},
+				{
+					name: "bio01_bio12_eufgis_round",
+					species: false,
+					units: true
+				},
+			]
+		},
+		tas_bio12: {
+			key: "bio01_bio12",
+			X: "env_climate_tas",
+			Y: "env_climate_bio12",
+			collections: [
+				{
+					name: "tas_bio12_chelsa_full",
+					species: false,
+					units: false
+				},
+				{
+					name: "tas_bio12_eu_full",
+					species: true,
+					units: false
+				},
+				{
+					name: "tas_bio12_eufgis_full",
+					species: false,
+					units: true
+				},
+				{
+					name: "tas_bio12_chelsa_round",
+					species: false,
+					units: false
+				},
+				{
+					name: "tas_bio12_eu_round",
+					species: true,
+					units: false
+				},
+				{
+					name: "tas_bio12_eufgis_round",
+					species: false,
+					units: true
+				},
+			]
+		}
+	},
+	settings: {
+		maxRuntime: 0,      // No limit.
+		timeout: 3600000    // One hour.
+	}
+})
